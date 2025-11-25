794ec646f27b7e22ff5746bb1e0aadffe70bbe39
GhazyUrbayani <19623060@std.stei.itb.ac.id>
2025-11-25 18:43:45 +0700

    Fix-ing Deploy Error

--- app/api/auth/session-check/route.ts
+++ app/api/auth/session-check/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-export async function GET(_req: NextRequest) {
+export async function GET() {
   try {
     const cookieStore = cookies();
     const voterSession = cookieStore.get("voter-session");
